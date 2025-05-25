f4fefb092a4d24dbe285af2e594ae409801f71e2
7gahunguflorian <gahungufloriannosimbola@gmail.com>
2025-05-25 06:28:29 +0200

    Update UserRepository.java

--- src/main/java/com/farm/delivery/farmapi/repository/UserRepository.java
+++ src/main/java/com/farm/delivery/farmapi/repository/UserRepository.java
@@ -18,4 +18,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
+
+    long countByRole(User.Role role);
 }
